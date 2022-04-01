74bab0c8c7ceb2d96a97fcff6337f5365b8520fa
shu20031026 <shu20031026@gmail.com>
2022-04-02 00:09:43 +0900

    add: 写真欄の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
-
+import MyFace from "./my-face.jpg" 
+import Introduction from "./introduction.json"
 const background = css`
-  background-color: #bbe2f1;
   width: 100%;
   height:100vh;
 `
 
-const backgroundTop = css`
-
+const pageTop = css`
+  background-color: #3a8daa;
+  width: 100%;
+  height: 50%;
 ` 
 
-const backgroundBottom = css`
+const imageWrapper = css`
+  margin-top: -200px;
+  padding-left: 50px;
+`
 
+const image = css`
+  border-radius: 50%;
+  border: 0.5rem solid white;
+`
+
+const pageBottom = css`
+  background-color: white;
+  width: 100%;
 `
 
 function App() {
+  const data = Introduction
   return (
   <>
     <div css={background}>
-      <div css={backgroundTop}></div>
-      <div css={backgroundBottom}></div>
+      <div css={pageTop}>
+      </div>
+      <div css={imageWrapper}>
+        <img src={MyFace} alt="かお" css={image} width="400px" height="400px"/>
+      </div>
+      <div css={pageBottom}>
+
+      </div>
     </div>
   </>
   );
