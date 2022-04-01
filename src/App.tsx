93824a63fbc9c474a74edb6f31a1e7d0a864bbea
shu20031026 <shu20031026@gmail.com>
2022-04-02 00:31:56 +0900

    add: 名前の追加

--- src/App.tsx
+++ src/App.tsx
@@ -28,18 +28,42 @@ const pageBottom = css`
   width: 100%;
 `
 
-function App() {
+const nameArea = css`
+  display: flex;
+  align-items: flex-end;
+  padding: 24px;
+`
+const kanjiName = css`
+  margin: 0;
+  margin-right: 12px;
+  font-size: 6.0rem;
+  color: white;
+  font-weight: bold;
+
+`
+const englishName = css`
+  margin: 0;
+  font-size: 4.0rem;
+  color: white;
+  margin-bottom: 12px;
+`
+
+const App = () => {
   const data = Introduction
   return (
   <>
     <div css={background}>
       <div css={pageTop}>
+        <div css={nameArea}>
+          <p css={kanjiName}>{`${data.name.lastName.kanji} ${data.name.firstName.kanji}`}</p>
+          <p css={englishName}>{`${data.name.firstName.english} ${data.name.lastName.english}`}</p>
+        </div>
+        
       </div>
       <div css={imageWrapper}>
         <img src={MyFace} alt="かお" css={image} width="400px" height="400px"/>
       </div>
       <div css={pageBottom}>
-
       </div>
     </div>
   </>
