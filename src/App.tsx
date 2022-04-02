1c669050da3bd84d9c09a40fca9b7eb42ba66a7f
shu20031026 <shu20031026@gmail.com>
2022-04-02 15:38:22 +0900

    fix: cssの切り分け

--- src/App.tsx
+++ src/App.tsx
@@ -3,150 +3,34 @@ import { css } from "@emotion/react"
 import MyFace from "./my-face.jpg" 
 import Twitter from "./twitter-icon.jpeg"
 import Introduction from "./introduction.json"
-
-const background = css`
-  width: 100%;
-  height:100vh;
-`
-
-const pageTop = css`
-  background-color: #3a8daa;
-  width: 100%;
-  height: 70%;
-` 
-
-const mainImageWrapper = css`
-  margin-top: -360px;
-  padding-left: 50px;
-`
-
-const mainImage = css`
-  border-radius: 50%;
-  border: 0.5rem solid white;
-`
-
-const pageBottom = css`
-  background-color: white;
-  width: 100%;
-  display: flex;
-`
-
-const nameArea = css`
-  display: flex;
-  color: white;
-  justify-content: space-between;
-`
-
-const nameWrapper = css`
-  align-items: flex-end;
-  padding: 24px;
-  display: flex;
-`
-
-const kanjiName = css`
-  margin: 0;
-  margin-right: 12px;
-  font-size: 6.0rem;
-  color: white;
-  font-weight: bold;
-`
-
-const englishName = css`
-  margin: 0;
-  font-size: 4.0rem;
-  color: white;
-  margin-bottom: 12px;
-`
-
-const twitterImage = css`
-  border-radius: 50%;
-  border: 0.5rem solid white;
-`
-
-const twitterIconWrapper = css`
-  margin-left: -30px;
-  margin-top: -35px;
-`
-
-const bottomSpace = css`
-  margin-top: -50px;
-  margin-left: -40px;
-  color: #3a8daa;
-`
-
-const nameOtherInfo = css`
-  margin-top: 60px;
-  margin-right: 100px;
-`
-
-const studentNumber = css`
-  margin: 0;
-  font-size: 1.8rem;
-`
-
-const specializeField = css `
-  margin: 0;
-  font-size: 1.8rem;
-`
-
-const favSpace = css`
-  margin-left: 500px;
-  color: white;
-`
-
-const favTitle = css`
-  font-size: 3rem;
-  font-weight: 700;
-`
-
-const favThing = css`
-  font-size: 1.6rem;
-`
-
-const favComment = css`
-  font-size: 1rem;
-  padding-left: 40px;
-`
-
-const hobbyThing = css`
-  font-size: 1.6rem;
-`
-
-const hobbyTitle = css`
-  font-size: 2rem;
-  padding-left: 160px;
-  font-weight: 700;
-`
-
-const hobbyContent = css`
-  font-size: 1rem;
-  padding-left: 40px;
-`
-
-const hobbyContainer = css`
-  padding-left: 180px;
-`
-
-const twitterObject = css`
-  text-align: center;
-  margin-left: 80px;
-  margin-top: -80px;
-  width: 160px;
-  height: 160px;
-  border-radius: 50%;
-  background-color: #3a8daa;
-  color: white;
-  border: 0.5rem solid white;
-`
-
-const twitterTitle = css`
-  font-weight:600;
-  margin: 10px;
-`
-
-const twitterContent = css`
-  margin:10px;
-`
+import { 
+  background, 
+  bottomSpace, 
+  englishName, 
+  favComment, 
+  favSpace, 
+  favThing, 
+  favTitle, 
+  hobbyContainer, 
+  hobbyContent, 
+  hobbyThing, 
+  hobbyTitle, 
+  kanjiName, 
+  mainImage, 
+  mainImageWrapper, 
+  nameArea, 
+  nameOtherInfo, 
+  nameWrapper, 
+  pageBottom, 
+  pageTop, 
+  specializeField, 
+  studentNumber, 
+  twitterContent, 
+  twitterIconWrapper, 
+  twitterImage, 
+  twitterObject, 
+  twitterTitle 
+} from "./style"
 
 const App = () => {
   const data = Introduction
---
+++ src/style.ts
@@ -0,0 +1,146 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+export const background = css`
+  width: 100%;
+  height:100vh;
+`
+
+export const pageTop = css`
+  background-color: #3a8daa;
+  width: 100%;
+  height: 70%;
+` 
+
+export const mainImageWrapper = css`
+  margin-top: -360px;
+  padding-left: 50px;
+`
+
+export const mainImage = css`
+  border-radius: 50%;
+  border: 0.5rem solid white;
+`
+
+export const pageBottom = css`
+  background-color: white;
+  width: 100%;
+  display: flex;
+`
+
+export const nameArea = css`
+  display: flex;
+  color: white;
+  justify-content: space-between;
+`
+
+export const nameWrapper = css`
+  align-items: flex-end;
+  padding: 24px;
+  display: flex;
+`
+
+export const kanjiName = css`
+  margin: 0;
+  margin-right: 12px;
+  font-size: 6.0rem;
+  color: white;
+  font-weight: bold;
+`
+
+export const englishName = css`
+  margin: 0;
+  font-size: 4.0rem;
+  color: white;
+  margin-bottom: 12px;
+`
+
+export const twitterImage = css`
+  border-radius: 50%;
+  border: 0.5rem solid white;
+`
+
+export const twitterIconWrapper = css`
+  margin-left: -30px;
+  margin-top: -35px;
+`
+
+export const bottomSpace = css`
+  margin-top: -50px;
+  margin-left: -40px;
+  color: #3a8daa;
+`
+
+export const nameOtherInfo = css`
+  margin-top: 60px;
+  margin-right: 100px;
+`
+
+export const studentNumber = css`
+  margin: 0;
+  font-size: 1.8rem;
+`
+
+export const specializeField = css `
+  margin: 0;
+  font-size: 1.8rem;
+`
+
+export const favSpace = css`
+  margin-left: 500px;
+  color: white;
+`
+
+export const favTitle = css`
+  font-size: 3rem;
+  font-weight: 700;
+`
+
+export const favThing = css`
+  font-size: 1.6rem;
+`
+
+export const favComment = css`
+  font-size: 1rem;
+  padding-left: 40px;
+`
+
+export const hobbyThing = css`
+  font-size: 1.6rem;
+`
+
+export const hobbyTitle = css`
+  font-size: 2rem;
+  padding-left: 160px;
+  font-weight: 700;
+`
+
+export const hobbyContent = css`
+  font-size: 1rem;
+  padding-left: 40px;
+`
+
+export const hobbyContainer = css`
+  padding-left: 180px;
+`
+
+export const twitterObject = css`
+  text-align: center;
+  margin-left: 80px;
+  margin-top: -80px;
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  background-color: #3a8daa;
+  color: white;
+  border: 0.5rem solid white;
+`
+
+export const twitterTitle = css`
+  font-weight:600;
+  margin: 10px;
+`
+
+export const twitterContent = css`
+  margin:10px;
+`
